222f4e866def3ef86ca46ce29c1165ed0956d0e4
Eduardo Fernandes Alves <eduardo.feralves@sempreceub.com>
2025-05-11 17:04:58 -0300

    Delete exercicios-Python directory

--- exercicios-Python/Questão2.py
+++
@@ -1,4 +0,0 @@
-massa = float(input("Digite seu peso em kg: "))
-dose_ideal = massa * 0.03
-print("Sua necessidade de água é:", dose_ideal, "litros por dia")
-
--- exercicios-Python/Questão4.py
+++
@@ -1,6 +0,0 @@
-valor1 = int(input("Digite o primeiro valor inteiro: "))
-valor2 = int(input("Digite o segundo valor inteiro: "))
-if valor1 < valor2:
-    print("Ordem crescente:", valor1, valor2)
-else:
-    print("Ordem crescente:", valor2, valor1)
